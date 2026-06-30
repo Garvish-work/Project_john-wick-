da8b2f5ca8ca068aa2b69cc93e024d97acc9d668
Garvish.Work <garvish.work@gmail.com>
2026-06-30 10:33:28 +0530

    Made enemy prefab and added ragdoll effect to enemy

--- Assets/_My Assets/Scripts/Enemy scripts/EnemyBodyPartSystem.cs
+++ Assets/_My Assets/Scripts/Enemy scripts/EnemyBodyPartSystem.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class EnemyBodyPartSystem : MonoBehaviour, IDamagable
 {
+    Rigidbody bone;
     [SerializeField] private Animator enemyAnimatior;
     [SerializeField] private EnemyHealthSystem healthSystem;
     [SerializeField] private BodyPart bodyPart;
@@ -10,20 +11,26 @@ public class EnemyBodyPartSystem : MonoBehaviour, IDamagable
     [SerializeField] private string dealthType;
     [SerializeField] private float damageMultiplier = 1;
 
+    private void Awake()
+    {
+        bone = GetComponent<Rigidbody>();   
+    }
+
     public void Damage(float damageTaken)
     {
         float health = healthSystem.GetHealth();
         health = Mathf.Max(0, health -= (damageTaken * damageMultiplier));
 
+        healthSystem.SetHealth(health);
+
         if (health <= 0) Death();
         else enemyAnimatior.SetTrigger("Hit");
-
-        healthSystem.SetHealth(health);
     }
 
     public void Death()
     {
-        enemyAnimatior.SetTrigger(dealthType);
+        enemyAnimatior.enabled = false;
+        bone.AddForce(healthSystem.GetPlayerTransform().transform.forward * 25 , ForceMode.Impulse);
     }
 }
 
--- Assets/_My Assets/Scripts/Enemy scripts/EnemyHealthSystem.cs
+++ Assets/_My Assets/Scripts/Enemy scripts/EnemyHealthSystem.cs
@@ -3,17 +3,52 @@ using UnityEngine;
 public class EnemyHealthSystem : MonoBehaviour
 {
     [SerializeField] private float health = 100;
+    [SerializeField] private Transform mainPlayer;
     [SerializeField] private Animator enemyAnimator;
+    [SerializeField] private Rigidbody[] chracterBones;
+
+    private void Awake()
+    {
+        SetRagdoll(false);
+    }
 
     public float GetHealth()
     {
-        return health;  
+        return health;
     }
 
     public void SetHealth(float _health)
     {
         health = _health;
         EnemyActions.EnemyGotHit?.Invoke(health, transform);
+
+        if (_health <= 0) SetRagdoll(true);
     }
-        
+
+    public Transform GetPlayerTransform()
+    {
+        return mainPlayer;
+    }
+
+    private void SetRagdoll(bool check)
+    {
+        switch (check)
+        {
+            case false:
+                enemyAnimator.enabled = true;
+                foreach (Rigidbody bone in chracterBones)
+                {
+                    bone.isKinematic = true;
+                }
+                break;
+
+            case true:
+                enemyAnimator.enabled = false;
+                foreach (Rigidbody bone in chracterBones)
+                {
+                    bone.isKinematic = false;
+                }
+                break;
+        }
+    }
 }
